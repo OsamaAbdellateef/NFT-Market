f782520e6598522dd0144e1084df2b0a2eec4b7d
OsamaAbellateef <osama.abd9888@gmail.com>
2022-02-10 23:08:35 +0200

    adding slider

--- pages/createNFT/index.js
+++ pages/createNFT/index.js
@@ -8,7 +8,7 @@ const CreatNFT = () => {
     <div>
       <Breadcrumb links={["home", "createNFT"]} />
 
-      <div className="py-4 ">
+      <div className=" py-4 ">
         {/*Form*/}
         <h1 className="mb-6 font-semibold text-4xl">Create Your NFT</h1>
         <p className="mb-8">
@@ -16,8 +16,8 @@ const CreatNFT = () => {
         </p>
 
         <form>
-          <div className=" grid xs:grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
+          <div className=" grid xs:grid-cols-1 md:grid-cols-2 gap-40">
+            <div className="">
               <div class="mb-6">
                 <label
                   for="NFT-title"
@@ -64,99 +64,131 @@ const CreatNFT = () => {
                   <span className="eth">ETH</span>
                 </div>
               </div>
-              <div class="mb-6">
-                <label
-                  for="seller"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Seller
+              <div className="mb-6">
+                <label className="block text-sm font-medium text-gray-700">
+                  upload photo
                 </label>
-                <input
-                  type="text"
-                  id="seller"
-                  class="bg-primary-light border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
+                <div className="bg-primary-light mt-1 mb-8 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                  <div className="space-y-1 text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    <div className="flex text-sm text-gray-600">
+                      <label
+                        style={{ background: "none" }}
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                      >
+                        <span
+                          style={{
+                            fontWeight: "bold",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          Upload a file
+                        </span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only"
+                        />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                    <p className="text-xs text-gray-500">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div class="mb-6">
+              <div className="mb-6">
                 <label
-                  for="creator"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  for="message"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
                 >
-                  Creator
+                  Your message
                 </label>
+                <textarea
+                  id="message"
+                  rows="4"
+                  class="bg-primary-light block p-2.5 w-full text-sm text-gray-900  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 row-span-6 col-span-6 "
+                  placeholder="Leave a comment..."
+                ></textarea>
                 <input
-                  type="text"
-                  id="creator"
-                  class="bg-primary-light border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="cursor-pointer mt-6 submit-button"
+                  type="submit"
                 />
               </div>
             </div>
             {/**/}
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                upload photo
-              </label>
-              <div className="bg-primary-light mt-1 mb-8 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                <div className="space-y-1 text-center">
-                  <svg
-                    className="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <div className="flex text-sm text-gray-600">
-                    <label
-                      style={{ background: "none" }}
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                    >
-                      <span
-                        style={{
-                          fontWeight: "bold",
-                          textDecoration: "underline",
-                        }}
-                      >
-                        Upload a file
-                      </span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        className="sr-only"
-                      />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
+            <div className="slider">
+              <div className="slider-container">
+                <div className="slider-img-box img-1">
+                  <div className="slider-details">
+                    <h1 className="font-extrabold mb-2">Abstr Gradient NFT</h1>
+                    <p>
+                      <img
+                        className=" mr-4 gorup-icon"
+                        src="./group-icon.png"
+                      />{" "}
+                      <span className="font-medium">4363733</span>
+                    </p>
                   </div>
-                  <p className="text-xs text-gray-500">
-                    PNG, JPG, GIF up to 10MB
-                  </p>
+                  <img className="slider-item" src="./slider2.png" />
+                  <img className="group-icon" src="./group.png" />
+                  <div className="lower-part">
+                    <div className="left-part">
+                      <span className="font-normal text-xs">Current bid</span>
+                      <p>
+                        <img
+                          style={{ display: "inline-block" }}
+                          src="./vector.png"
+                          className="mr-1 "
+                        />
+                        <span className="font-semibold">0.25 ETH</span>
+                      </p>
+                    </div>
+                    <div className="right-part">
+                      <span className="font-normal text-xs">Ends in</span>
+                      <p>
+                        <span className="font-semibold mr-1">
+                          12
+                          <span className="font-light">h</span>
+                        </span>
+                        <span className="font-semibold mr-1">
+                          43
+                          <span className="font-light">m</span>
+                        </span>
+                        <span className="font-semibold mr-1">
+                          42
+                          <span className="font-light">s</span>
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="slider-img-box img-2">
+                  <img className="slider-item" src="./slider1.png" />
+                </div>
+                <div className="slider-img-box img-3">
+                  <img className="slider-item" src="./slider3.png" />
                 </div>
               </div>
-              <label
-                for="message"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-              >
-                Your message
-              </label>
-              <textarea
-                id="message"
-                rows="4"
-                class="bg-primary-light block p-2.5 w-full text-sm text-gray-900  rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 row-span-6 col-span-6 "
-                placeholder="Leave a comment..."
-              ></textarea>
             </div>
             {/**/}
           </div>
-
-          <input className="cursor-pointer mt-6 submit-button" type="submit" />
         </form>
       </div>
     </div>
